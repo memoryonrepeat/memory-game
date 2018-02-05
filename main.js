8fe2b03bc77601fe50c75043f1b0b9aa56fb0573
Thanh Nguyen <tienthanh8490@gmail.com>
2018-02-05 14:45:05 +0700

    use absolute path

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-var API_PATH = "api/goal.js";
+var API_PATH = __dirname+"/api/goal.js";
 var fs = require('fs');
 
 // Parse DB and hold its info in an object
 function parseDB() {
-	return JSON.parse(fs.readFileSync(API_PATH, 'utf8')).series.short_description;
+	return JSON.parse(fs.readFileSync(API_PATH, 'utf8'));
 }
 
 var raw_data = parseDB();
+
 
 console.log(raw_data);
