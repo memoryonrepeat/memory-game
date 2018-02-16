d6d1536322efb123ef4786fd77a9d68b3538564b
Thanh Nguyen <tienthanh8490@gmail.com>
2018-02-17 01:42:16 +0700

    refactor main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,3 @@
-var SIZE = 8;
 var currentlyFlipped = [];
 
 // Assign an index number to each goal for faster reference during randomization
@@ -11,20 +10,20 @@ function transformData(goal_items){
 };
 
 // Given board size N<=14, assign randomly each board to a goal item and a language
-function initializeBoard(){
+function randomizeBoard(size){
 	var goal_pot = []; // Goal candidates
 	var square_pot = []; // Square candidates
 
-	for (var i=0; i<SIZE*SIZE/2; i++){
+	for (var i=0; i<size*size/2; i++){
 		goal_pot.push(i);
 	}
-	for (var i=0; i<SIZE*SIZE; i++){
+	for (var i=0; i<size*size; i++){
 		square_pot.push(i);
 	}
 
 	// console.log(goal_pot, square_pot);
 	var board = {};
-	for (var i=0; i<SIZE*SIZE/2; i++){
+	for (var i=0; i<size*size/2; i++){
 
 		var goal_index = Math.floor(Math.random() * goal_pot.length);
 
@@ -43,38 +42,28 @@ function initializeBoard(){
 		square_pot.splice(square_index,1);
 		goal_pot.splice(goal_index,1);
 	}
+
+	board.size = size;
 	return board;
 };
 
 // Map the initialized board states to goals provided by API
 function mapBoardGoals(board, goals){
-	// console.log(goals);
 	for (var key in board){
-		// console.log(board[key]);
-		if (board[key][1]=='en'){
-			board[key] = [goals[board[key][0]].item.cue, board[key]];
-		}
-		else{
-			board[key] = [goals[board[key][0]].item.response, board[key]];
+		if (key !== 'size'){
+			if (board[key][1]=='en'){
+				board[key] = [goals[board[key][0]].item.cue, board[key]];
+			}
+			else{
+				board[key] = [goals[board[key][0]].item.response, board[key]];
+			}
 		}
 	}
-	board.size = SIZE;
 	return board;
 };
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+function initializeBoard(size, data){
+	console.log(randomizeBoard(size));
+	return mapBoardGoals(randomizeBoard(size), transformData(data));
 };
-
-/*var goals = transformData(parseDB().goal_items);
-var board = mapBoardGoals(initializeBoard(), transformData(parseDB().goal_items));*/
-
-//console.log(goals);
-
-// console.log(getRandomInt(100));
-
-// console.log(initializeBoard());
-
-// console.log(mapBoardGoals(initializeBoard(), transformData(parseDB().goal_items)));
-
 
